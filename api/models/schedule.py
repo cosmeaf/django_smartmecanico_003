e4e5202f081b04d0ae66cdd01d3eccb4abca63d1
Cosme Alves <cosme.alex@gmail.com>
2023-01-09 21:03:43 -0300

    REF: Refactory Modal Schedule

--- api/models/schedule.py
+++ api/models/schedule.py
@@ -9,10 +9,9 @@ from django.contrib.auth.models import User
 
 class Schedule(Base, models.Model):
     user = models.ForeignKey(User, verbose_name='Usuário', on_delete=models.CASCADE)
-    address = models.ForeignKey(Address, verbose_name='Endereço', on_delete=models.PROTECT, related_name='Address', related_query_name="Addres")
-    vehicle = models.ForeignKey(Vehicle, verbose_name='Veículo', on_delete=models.PROTECT, related_name='Vehicle', related_query_name="Vehicle")
-    service = models.ForeignKey(Service, verbose_name='Serviço', on_delete=models.PROTECT, related_name='Service', related_query_name="Service")
-    #hour = models.ForeignKey(HourService, verbose_name='HourService', on_delete=models.PROTECT, related_name='HourService', related_query_name="HourService")
+    address = models.ForeignKey(Address, verbose_name='Endereço', on_delete=models.CASCADE, related_name='Address', related_query_name="Addres")
+    vehicle = models.ForeignKey(Vehicle, verbose_name='Veículo', on_delete=models.CASCADE, related_name='Vehicle', related_query_name="Vehicle")
+    service = models.ForeignKey(Service, verbose_name='Serviço', on_delete=models.CASCADE, related_name='Service', related_query_name="Service")
     hour = models.CharField('Hora do Serviço', max_length=5)
     day = models.DateField('Data do Serviço', help_text='Escolha data disponível')
 
